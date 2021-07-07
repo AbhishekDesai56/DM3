4d6fb489e687dec67590b1b4fdc5fd0be9f8eb51
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-07-08 01:49:17 +0530

    [Abhishek] Refactor Code to support List in place of iterator

--- src/main/java/censusanalyser/OpenCSVBuilder.java
+++ src/main/java/censusanalyser/OpenCSVBuilder.java
@@ -5,15 +5,25 @@ import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 public class OpenCSVBuilder<E> implements ICSVBuilder {
     public Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuiderException {
+        return this.getCSVBean(reader, csvClass).iterator();
+    }
+
+    @Override
+    public List getCSVFileList(Reader reader, Class csvClass) throws CSVBuiderException {
+        return this.getCSVBean(reader, csvClass).parse();
+    }
+
+    private CsvToBean<E> getCSVBean(Reader reader, Class csvClass) throws CSVBuiderException {
         try {
             CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
             csvToBeanBuilder.withType(csvClass);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-            return csvToBean.iterator();
+            return csvToBeanBuilder.build();
         } catch (IllegalStateException e) {
             throw new CSVBuiderException(e.getMessage(),
                     CSVBuiderException.ExceptionType.UNABLE_TO_PARSE);
